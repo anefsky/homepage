a1de65949e83064e1f57f00d667c58bcbf4cf2be
Andrew Nefsky <anefsky@gmail.com>
2015-12-09 16:49:57 -0500

    More angular changes

--- js/site.js
+++ js/site.js
@@ -1,76 +1,16 @@
-$(function() {
-	var app = new App();
-	app.start();
-});
-
-var App = function() {
-	console.log("in App constructor");
-
-	this.start = function() {
-
-		console.log("in start");
-
-		var appModel = new AppModel();
-		var appView = new AppView({
-			model : appModel
-		});
-	}
+var populateBackgroundImage = function(selector, image) {
+	var selectorEl = document.querySelector(selector);
+	selectorEl.style.backgroundImage = 'url(' + image + ')';
 }
 
-var AppModel = Backbone.Model.extend( {
-/*	populateScreen : function(theData) {
-		console.log('In populateScreen');
+var myApp = angular.module("myApp", []);
 
-		$.ajax( {
-			url: 'data/person_data.json',
-			success: function(result) {
-				console.log("ajax success, result: ", result)
-				var theData = result;
+myApp.controller('myCntl', function($scope, $http) {
+	$http.get("data/person.json")
+	.then(function(response) {
+		$scope.person = response.data.person;
+		populateBackgroundImage('#profile .my-pic', $scope.person.personImage);
+		populateBackgroundImage('header', $scope.person.heroImage);
 
-				var name = theData.fullname;
-				var role = theData.role;
-
-			},
-			error : function(xhr, status, error) {
-				console.log('ajax call failed: ' + error);
-			}
-
-		});
-
-	},
-*/	getName : function() {
-		return "Fred Flintstone"
-	},
-	getRole : function() {
-		return "Rock collector"
-	}
+	});
 });
-
-var AppView = Backbone.View.extend( {
-	el$: $('#branding'),
-//	template: _.template($('#test-template').html()),
-	initialize: function() {
-		console.log('in AppView initialize');
-
-		this.render();
-	},
-	render : function() {
-		console.log('in AppView render()');
-//		$(this.el$).append(this.template({
-//		this.template({
-//			who: this.model.getName(),
-//			what: this.model.getRole()
-//		}
-
-		$(this.el$).append('#test-template').html( {
-//			who: this.model.getName(),
-//			what: this.model.getRole()
-		})
-
-	}
-
-});
-
-
-
-
